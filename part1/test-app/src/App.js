bcd31d348206e0ea76efe881abfe8bffb13b5d28
redacted24 <huangq4@outlook.com>
2023-08-11 20:46:48 -0400

    Spread syntax

--- part1/test-app/src/App.js
+++ part1/test-app/src/App.js
@@ -1,36 +1,22 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [clicks, setClicks] = useState ({
+    left: 0,
+    right: 0,
+  })
 
-  const [ counter, setCounter ] = useState(0)
+  const handleLeftClick = () => {setClicks({...clicks, left: clicks.left + 1})}
 
-  const plusOne = () => {setCounter(counter + 1)}
-  const plusTwo = () => {setCounter(counter + 2)}
-  const minusOne = () => {setCounter(counter - 1)}
-  const setZero = () => {setCounter(0)}
+  const handleRightClick = () => {setClicks ({...clicks, right: clicks.right + 1})}
 
   return (
-    <>
-      <div>
-        <Display counter = {counter}/>
-        <Button handleClick = {plusOne} text='Plus one'/>
-        <Button handleClick = {plusTwo} text='Plus two'/>
-        <Button handleClick = {minusOne} text='Minus one'/>
-        <Button handleClick = {setZero} text='Reset'/>
-      </div>
-    </>
-  )
-}
-
-const Display = ({counter}) => {
-  console.log("Display Component-Rerendered.")
-  return (<div>{counter}</div>)
-}
-
-const Button = ({handleClick, text}) => {
-  console.log("Button Component-Rerendered.")
-  return (
-    <button onClick={handleClick}>{text}</button>
+    <div>
+      {clicks.left}
+      <button onClick = {handleLeftClick}>Left</button>
+      <button onClick = {handleRightClick}>Right</button>
+      {clicks.right}
+    </div>
   )
 }
 
