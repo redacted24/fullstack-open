a557bec94dea04078c97b6fbb7a5c32fc6df2b7a
redacted24 <huangq4@outlook.com>
2023-08-11 20:00:11 -0400

    Passing state to child components

--- part1/test-app/src/App.js
+++ part1/test-app/src/App.js
@@ -1,30 +1,36 @@
 import { useState } from 'react'
 
 const App = () => {
+
   const [ counter, setCounter ] = useState(0)
-  const plusOne = () => {
-    setCounter(counter + 1)
-  }
-  const plusTwo = () => {
-    setCounter(counter + 2)
-  }
-  const setZero = () => {
-    setCounter(0)
-  }
+
+  const plusOne = () => {setCounter(counter + 1)}
+  const plusTwo = () => {setCounter(counter + 2)}
+  const minusOne = () => {setCounter(counter - 1)}
+  const setZero = () => {setCounter(0)}
+
   return (
     <>
-      <p>{counter}</p>
       <div>
-        <button onClick={plusOne}>Add one!</button>
-        <button onClick={plusTwo}>Add two!</button>
-        <button onClick={setZero}>Reset</button>
+        <Display counter = {counter}/>
+        <Button handleClick = {plusOne} text='Plus one'/>
+        <Button handleClick = {plusTwo} text='Plus two'/>
+        <Button handleClick = {minusOne} text='Minus one'/>
+        <Button handleClick = {setZero} text='Reset'/>
       </div>
     </>
   )
 }
 
+const Display = (props) => {
+  return (<div>{props.counter}</div>)
+}
 
-
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
 
 export default App;
 
