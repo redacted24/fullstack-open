2c16a26867ccac0b24d7d88e21725abb2adecfd4
redacted24 <huangq4@outlook.com>
2023-08-18 12:16:44 -0400

    Update of State is Asynchronous

--- part1/test-app/src/App.js
+++ part1/test-app/src/App.js
@@ -6,25 +6,43 @@ const App = () => {
     right: 0,
   })
 
-  const handleLeftClick = () => {setClicks({...clicks, left: clicks.left + 1})}
+  const [allClicks, setAll] = useState([])
 
-  const handleRightClick = () => {setClicks ({...clicks, right: clicks.right + 1})}
+  const  [total, setTotal] = useState(0)
 
-  const handleTwoClick = () => {setClicks({left: clicks.left + 1, right: clicks.right + 1})}
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = clicks.left + 1
+      setClicks({...clicks, left: updatedLeft})
+      setTotal(updatedLeft + clicks.right)
+  }
 
-  const handleRemoveClick = () => {setClicks({left: clicks.left - 1, right: clicks.right - 1})}
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = clicks.right + 1
+      setClicks({...clicks, right: updatedRight})
+      setTotal(updatedRight + clicks.left)
+  }
+
+  const handleReset = () => {
+    setClicks({left: 0, right: 0})
+    setAll([])
+    console.log(allClicks)
+    setTotal(0)
+    
+  }
+
 
   return (
     <div>
       {clicks.left}
       <button onClick = {handleLeftClick}>Left</button>
-{/* The Onclick is an event, and handleLeftClick is an event handler. */}
       <button onClick = {handleRightClick}>Right</button>
       {clicks.right}
-      <p></p>
-      <button onClick = {handleTwoClick}>Add Both</button>
-      <p></p>
-      <button onClick = {handleRemoveClick}>Remove from Both</button>
+      <p>{allClicks.join('')}</p>
+      <p>Total: {total}</p>
+      <button onClick = {handleReset}>Reset</button>
+
     </div>
   )
 }
