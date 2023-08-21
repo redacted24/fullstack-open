f8cfc9d0e4e32e7bf9fe2841cb3f35c17c2e7f2f
redacted24 <huangq4@outlook.com>
2023-08-21 17:18:57 -0400

    step 1.7'

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,10 +4,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [score, setScore] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGood = () => {
+    const updatedGood = good + 1
+    const GoodScore = score + 1
+    setGood(updatedGood)
+    setScore(GoodScore)
+    console.log("Good count:" + updatedGood, "Score count:" + GoodScore)
+  }
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1
+    const NeutralScore = score
+    setNeutral(updatedNeutral)
+    setScore(NeutralScore)
+    console.log("Neutral count:" + updatedNeutral, "Score count:" + NeutralScore)
+  }
+
+  const handleBad = () => {
+    const updatedBad = bad + 1
+    const BadScore = score - 1
+    setBad(updatedBad)
+    setScore(BadScore)
+    console.log("Bad count:" + updatedBad, "Score count:" + BadScore)
+  }
 
   const Buttons = ({handleClick, text, number}) => <button onClick = {handleClick}>{text} {number}</button>
 
@@ -18,6 +38,7 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>score: {score}</p>
       </div>
     )
   }
