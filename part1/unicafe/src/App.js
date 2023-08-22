64c82803018475ad40b521e336d8b27f1feabe4c
redacted24 <huangq4@outlook.com>
2023-08-22 19:21:21 -0400

    final step 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,32 +4,45 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [score, setScore] = useState(0)
+  const [all, setall] = useState(0)
 
   const handleGood = () => {
     const updatedGood = good + 1
-    const GoodScore = score + 1
+    const Goodall = all + 1
     setGood(updatedGood)
-    setScore(GoodScore)
-    console.log("Good count:" + updatedGood, "Score count:" + GoodScore)
+    setall(Goodall)
+    console.log("Good count:" + updatedGood, "all count:" + Goodall)
   }
   const handleNeutral = () => {
     const updatedNeutral = neutral + 1
-    const NeutralScore = score
+    const Neutralall = all + 1
     setNeutral(updatedNeutral)
-    setScore(NeutralScore)
-    console.log("Neutral count:" + updatedNeutral, "Score count:" + NeutralScore)
+    setall(Neutralall)
+    console.log("Neutral count:" + updatedNeutral, "all count:" + Neutralall)
   }
 
   const handleBad = () => {
     const updatedBad = bad + 1
-    const BadScore = score - 1
+    const Badall = all + 1
     setBad(updatedBad)
-    setScore(BadScore)
-    console.log("Bad count:" + updatedBad, "Score count:" + BadScore)
+    setall(Badall)
+    console.log("Bad count:" + updatedBad, "all count:" + Badall)
   }
 
   const Buttons = ({handleClick, text, number}) => <button onClick = {handleClick}>{text} {number}</button>
+
+  const Average = () => {
+    let average = (good-bad)/all
+    if (good, bad, neutral === 0) {
+      return (
+        <p>Average: please click a button</p>
+      )
+    }
+
+    return (
+      <p>Average: {average}</p>
+    )
+  }
 
   const Statistics = () => {
     return(
@@ -38,7 +51,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
-        <p>score: {score}</p>
+        <p>all: {all}</p>
+        <Average/>
+        <p>positive {(good/all)*100}%</p>
       </div>
     )
   }
