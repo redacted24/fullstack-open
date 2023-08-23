5fac72dbc10c22dc6fcdd2c7b156abddc651da4f
redacted24 <huangq4@outlook.com>
2023-08-23 11:35:29 -0400

    removed components nested in other components, need to try to fix the rest of my code

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,53 @@
 import { useState } from 'react'
 
+const Buttons = ({handleClick, text, number}) => <button onClick = {handleClick}>{text} {number}</button>
+
+const Average = (props) => {
+  let average = (props.good-props.bad)/props.all
+  if (props.all === 0) {
+    return (
+      <p>Average: please click a button</p>
+    )
+  }
+  return (
+    <p>Average: {average}</p>
+  )
+}
+
+const Positive = (props) => {
+  let positive = (props.good/props.all)*100
+  if (props.all === 0) {
+    return (
+      <p>positive: please click a button</p>
+    )
+  }
+  return (
+    <p>positive: {positive} %</p>
+  )
+}
+
+const Statistics = (props) => {
+  return(
+    <div>
+      <h2>statistics</h2>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all: {props.all}</p>
+      <Average/>
+      <Positive/>
+    </div>
+  )
+}
+
+const Header = ({text}) => {
+  return(
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -9,6 +57,7 @@ const App = () => {
   const handleGood = () => {
     const updatedGood = good + 1
     const Goodall = all + 1
+    console.log(updatedGood)
     setGood(updatedGood)
     setall(Goodall)
     console.log("Good count:" + updatedGood, "all count:" + Goodall)
@@ -20,61 +69,13 @@ const App = () => {
     setall(Neutralall)
     console.log("Neutral count:" + updatedNeutral, "all count:" + Neutralall)
   }
-
+  
   const handleBad = () => {
     const updatedBad = bad + 1
     const Badall = all + 1
     setBad(updatedBad)
     setall(Badall)
     console.log("Bad count:" + updatedBad, "all count:" + Badall)
-  }
-
-  const Buttons = ({handleClick, text, number}) => <button onClick = {handleClick}>{text} {number}</button>
-
-  const Average = () => {
-    let average = (good-bad)/all
-    if (all === 0) {
-      return (
-        <p>Average: please click a button</p>
-      )
-    }
-    return (
-      <p>Average: {average}</p>
-    )
-  }
-
-  const Positive = () => {
-    let positive = (good/all)*100
-    if (all === 0) {
-      return (
-        <p>positive: please click a button</p>
-      )
-    }
-    return (
-      <p>positive: {positive}</p>
-    )
-  }
-
-  const Statistics = () => {
-    return(
-      <div>
-        <h2>statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all: {all}</p>
-        <Average/>
-        <Positive/>
-      </div>
-    )
-  }
-
-  const Header = ({text}) => {
-    return(
-      <div>
-        <h1>{text}</h1>
-      </div>
-    )
   }
 
   return(
@@ -87,4 +88,5 @@ const App = () => {
     </>
   )
 }
+
 export default App;
