2df9ec0893e4641a20723b76e43c7ce9fdeaf11f
redacted24 <huangq4@outlook.com>
2023-09-04 18:13:02 -0400

    sync

--- part2/testing/src/App.jsx
+++ part2/testing/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Note from './components/Note'
 
-const App = () => {
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
   return (
     <div>
-      <p>Hello world</p>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
 
-export default App
+export default App 
