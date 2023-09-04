f0a7d74d047547440fb9afac88b6b8d21a321061
redacted24 <huangq4@outlook.com>
2023-09-04 14:41:23 -0400

    vite setup

---
+++ part2/testing/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
