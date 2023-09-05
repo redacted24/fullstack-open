3034b8815dca5de65d4053890a3723a167465bcb
redacted24 <huangq4@outlook.com>
2023-09-04 23:21:50 -0400

    phonebook step 2 done (2.7)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,31 +2,49 @@ import { useState } from 'react'
 import Numbers from './components/Numbers'
 
 const App = () => {
-
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
       id: 1}
   ]) 
-
+  // State handling the input value.
   const [newName, setNewName] = useState('')
 
+  // Button Press Event Handler.
   const addName = (event) => {
     event.preventDefault()
+
+    // Creation of a new profile (name + id).
     const newProfile = {
       name: newName,
       id: persons.length + 1,
     }
-    if (persons.includes({name: newName}) === false) {
-      setPersons(persons.concat(newProfile))
+    const updatedPersons = persons.concat(newProfile)
+
+    // Mapping out the names of the persons array, in order to compare with the input.
+    const namesArray = persons.map((element) => element.name)
+    console.log(namesArray)
+
+    // Checking if the entered name already exists, or if the name entered is blank.
+    if (namesArray.includes(newName) === true) {
+      alert(`${newName} is already in the phonebook.`)
+    } 
+    else if (newName === '') {
+      alert('Please enter a name.')
+    }
+    else {
+      setPersons(updatedPersons)
     }
     setNewName('')
-  }
 
+
+  }
+  // Input Event Handler
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  // App Return
   return (
     <div>
       <h2>Phonebook</h2>
